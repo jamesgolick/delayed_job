bec5f39c94f7c7c61772e1fd11536b90d1deadda
James Golick <james@giraffesoft.ca>
2008-11-08 09:19:10 -0500

    add my script that drops a pid and preloads models, and my patched worker that accepts a pid to attempt to clean up on exit

--- lib/delayed/worker.rb
+++ lib/delayed/worker.rb
@@ -1,18 +1,21 @@
 module Delayed
   class Worker
     SLEEP = 5
+    attr_reader :pid_file
 
     def initialize(options={})
       @quiet = options[:quiet]
       Delayed::Job.min_priority = options[:min_priority] if options.has_key?(:min_priority)
       Delayed::Job.max_priority = options[:max_priority] if options.has_key?(:max_priority)
+      @pid_file = options[:pid_file]
     end                                                                          
 
     def start
       say "*** Starting job worker #{Delayed::Job.worker_name}"
 
-      trap('TERM') { say 'Exiting...'; $exit = true }
-      trap('INT')  { say 'Exiting...'; $exit = true }
+      worker = self
+      trap('TERM') { say 'Exiting...'; worker.cleanup_pid_file; $exit = true }
+      trap('INT')  { say 'Exiting...'; worker.cleanup_pid_file; $exit = true }
 
       loop do
         result = nil
@@ -40,5 +43,10 @@ module Delayed
       RAILS_DEFAULT_LOGGER.info text
     end
 
+    protected
+      def cleanup_pid_file
+        File.delete(pid_file) if !pid_file.nil? && File.exists?(pid_file)
+      end
   end
 end
+
